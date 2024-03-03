26121de2a712a49fabe66f1bc6a9e4bec59e4445
czr <928343544@qq.com>
2024-03-03 08:28:22 +0000

    feat:[1.1] mac platform

--- test/test_fiber.cpp
+++ test/test_fiber.cpp
@@ -14,10 +14,9 @@ void run_in_fiber() {
 /// each thread have a main fiber created by Fiber::GetThis()
 /// [Server::Fiber::ptr fiber(new Server::Fiber(run_in_fiber))] can create a sub fiber
 /// sub_fiber->swapIn(); //switch sub_fiber execute, and suspend main fiber
-
 int main() {
-    /// init main fiber
     {
+        /// init main fiber
         Server::Fiber::GetThis();
         LOGI(g_logger) << "main fiber begin";
         ///create a sub fiber with func
